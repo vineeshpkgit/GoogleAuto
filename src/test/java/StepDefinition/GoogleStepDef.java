367fbcaba9ca93f8a0a9ced8ec1f9344a30db817
VineeshPadinjareKamm <vpadinjare@allivet.com>
2025-09-24 08:41:54 +0000

    ok

--- src/test/java/StepDefinition/GoogleStepDef.java
+++ src/test/java/StepDefinition/GoogleStepDef.java
@@ -1,31 +1,42 @@
 package StepDefinition;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GoogleStepDef {
+
     WebDriver driver;
+
+    @Before
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless"); // For CI environments
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--user-data-dir=/tmp/profile-" + System.currentTimeMillis());
+
+        driver = new ChromeDriver(options);
+    }
 
     @Given("I open the Google homepage")
     public void openGoogleHomepage() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
         driver.get("https://www.google.com");
     }
 
     @When("I search for {string}")
     public void searchForKeyword(String keyword) {
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys(keyword);
-        searchBox.sendKeys(Keys.RETURN);
+        driver.findElement(By.name("q")).sendKeys(keyword + Keys.ENTER);
     }
 
-    @Then("I should see search results")
-    public void verifySearchResults() {
-        boolean resultsPresent = driver.findElements(By.cssSelector("div.g")).size() > 0;
-        assert resultsPresent : "No search results found!";
-        driver.quit();
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            //driver.quit();
+        }
     }
 }
